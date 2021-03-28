1c6478fb6e18ccc0d71cfb9883f2f2548b376710
AlexandrePereira0011 <alexandrepereira0011@gmail.com>
2021-03-29 00:28:44 +0100

    versao5

--- aulas/src/app/ev-and-ref/ev-and-ref.component.ts
+++ aulas/src/app/ev-and-ref/ev-and-ref.component.ts
@@ -11,9 +11,33 @@ export class EvAndRefComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  largura:number;
+  comprimento:number;
   cor:string="white";
+  clicked:Boolean = false;
   
   changecolor(corR:string){
     this.cor=corR;
+  }
+
+  rodar(referencia){
+     referencia.classList.add("rodar");
+     setTimeout(() => {
+      referencia.classList.remove("rodar");
+      },4000);
+  }
+
+  cores(referencia){
+    referencia.classList.add("mudarCor");
+    document.getElementById("botao2").innerText="Parar";
+    document.getElementById("botao2").addEventListener("click", ()=>{referencia.classList.toggle("mudarCor");document.getElementById("botao2").innerText="Mudar Cor";});
+ }
+
+  desformar(referencia){
+    this.comprimento= (Math.random() * 999) + 1;
+    this.largura= (Math.random() * 599) + 1;
+    referencia.style.width = this.comprimento+"px";
+    referencia.style.height= this.largura+"px";
   }
 }
